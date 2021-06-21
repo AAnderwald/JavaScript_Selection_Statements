f132fc105bb534113c342af9a1bba4fb9e7600d8
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-06-21 15:10:03 -0400

    notes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Hello World!\n==========\n");
 console.log("Follow the steps in the README.md file to complete the exercises:\n==========\n");
-
+/*
 // Exercise 1
  
 let userInput = window.prompt("Guess my favorite number: (hint it's between 1 and 30");
@@ -84,4 +84,51 @@ switch (month2) {
     default:
         console.log("Didja forget the month?");
 }
+*/
+
+/*
+//loops
+let continueLoopWhileAbove0 = 10;
+
+//while
+// will loop 0 or more times; values the condition first 
+while (continueLoopWhileAbove0 ) {
+    console.log(continueLoopWhileAbove0);
+    continueLoopWhileAbove0--;
+}
+
+// reads it as While (10 == True) then chagne while (9 == true) and will keep going unitl 0 == ture
+
+//do/while
+// will loop at least once, then as many times as the condition is true; so will loop 1 or more times
+do {
+    console.log(continueLoopWhileAbove0);
+    continueLoopWhileAbove0--;
+} while (continueLoopWhileAbove0);
+
+//for
+// the let here is only for this for statement and not effect anything else; 
+//delcare variable at ten and evaluate until it gets to zero
+
+for (let continueLoopWhileAbove0 = 10; 
+        continueLoopWhileAbove0;    
+        continueLoopWhileAbove0--)
+        {
+    console.log(continueLoopWhileAbove0)
+    }
+*/
+
+/*
+
+//assign functions to a variable where as name is the parameters
+const printName = funciton (name) {
+    cosole.log(name);
+};
+
+//declare a function
+funciont printName2(name) {
+    console.log(name);
+}
+*/
+
 
